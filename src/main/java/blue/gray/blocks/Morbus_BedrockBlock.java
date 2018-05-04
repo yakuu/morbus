b255191ab2dae70da6e48337d5a37248d5da7dd9
Yakuu <saleschjohn@gmail.com>
2018-05-04 23:43:06 +0200

    Added Morbus Bedrock+Ore

---
+++ src/main/java/blue/gray/blocks/Morbus_BedrockBlock.java
@@ -0,0 +1,20 @@
+package blue.gray.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class Morbus_BedrockBlock extends BlockBase 
+{
+	public Morbus_BedrockBlock(String Name, Material material) 
+	{
+		super(Name, material);
+		setSoundType(SoundType.ANVIL);
+		setHardness(31.5F);
+		setResistance(1300F);
+		setHarvestLevel("pickaxe", 2);
+		setLightLevel(1F);
+		//setLightOpacity(1);
+		setBlockUnbreakable();
+	}
+
+}
--- src/main/java/blue/gray/init/ModBlocks.java
+++ src/main/java/blue/gray/init/ModBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import blue.gray.blocks.BlockBase;
+import blue.gray.blocks.Morbus_BedrockBlock;
 import blue.gray.blocks.Morbus_BrickBlock;
 import blue.gray.blocks.Morbus_DirtBlock;
 import net.minecraft.block.Block;
@@ -15,6 +16,8 @@ public class ModBlocks
 	
 	public static final Block MORBUS_DIRT_BLOCK = new Morbus_DirtBlock("morbus_dirt_block", Material.GRASS);
 	public static final Block MORBUS_BRICK_BLOCK = new Morbus_BrickBlock("morbus_brick_block", Material.GROUND);
+	public static final Block MORBUS_BEDROCK_BLOCK = new Morbus_BedrockBlock("morbus_bedrock_block", Material.GROUND);
+	public static final Block MORBUS_ORE_BLOCK = new Morbus_BrickBlock("morbus_ore_block", Material.GROUND);
 	
 	
 }
