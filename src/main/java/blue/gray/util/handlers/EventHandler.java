ea55b412321cf73b271f16408f8a21e93b73f9c9
Yakuu <saleschjohn@gmail.com>
2018-05-04 23:15:14 +0200

    removed spam packeages from testing

--- src/main/java/blue/gray/util/handlers/EventHandler.java
+++ src/main/java/blue/gray/util/handlers/EventHandler.java
@@ -3,23 +3,13 @@ package blue.gray.util.handlers;
 import java.util.Random;
 
 import blue.gray.init.ModItems;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.monster.EntityCaveSpider;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants.NBT;
-import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 
 public class EventHandler {
 
