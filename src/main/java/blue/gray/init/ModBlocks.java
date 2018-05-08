972c80c610c6d2ee87f43e02202c1a55404953a5
Johnny <yakuu@live.de>
2018-05-08 06:51:36 +0200

    Update ModBlocks.java

--- src/main/java/blue/gray/init/ModBlocks.java
+++ src/main/java/blue/gray/init/ModBlocks.java
@@ -13,7 +13,8 @@ import net.minecraft.block.material.Material;
 public class ModBlocks 
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	
+	//Material Error inside a Pack for some reason https://github.com/yakuu/morbus/issues/1
+	//On Eclipse everything is fine :)
 	public static final Block MORBUS_DIRT_BLOCK = new Morbus_DirtBlock("morbus_dirt_block", Material.GRASS);
 	public static final Block MORBUS_BRICK_BLOCK = new Morbus_BrickBlock("morbus_brick_block", Material.GROUND);
 	public static final Block MORBUS_BEDROCK_BLOCK = new Morbus_BedrockBlock("morbus_bedrock_block", Material.GROUND);
